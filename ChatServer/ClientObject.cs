42ec2c882fb6d18b2ece2dd6acdc6a3e36945f63
matyc <matyc777@gmail.com>
2018-04-11 20:52:11 +0300

    Обновлённый

---
+++ ChatServer/ClientObject.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    public class ClientObject
+    {
+        protected internal string ClientName { get; private set; }//его принимаем за уникальное имя
+        protected internal NetworkStream Stream { get; private set; }
+        string CompanionName;
+        TcpClient client { get; }
+        Server server; // сервер
+
+        public ClientObject(TcpClient tcpClient, Server serverObject, string ClientName)
+        {
+            this.ClientName = ClientName;
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+            Stream = client.GetStream();//возможно лучше засунуть в метод Process()
+        }
+
+        public void Process()
+        {
+            try
+            {
+                string message = GetMessage();
+                //тут должен быть парсинг сообщения
+                string Name = "";//допустим мы получили имя с парсинга для отправки запроса на чаттинг
+
+                switch(message)
+                {
+                    case "0":// если клиент захотел создать чат(он прислал имя Челика с которым хочет чатиться)
+                        foreach(ClientObject clientObj in server.GetClients)// тогда ищем в онлайне этого челика и отправляем ему запрос
+                        {
+                            if (Name == clientObj.ClientName) server.SendMessage("I wanna chatting with you" + ClientName, clientObj.client);
+                        }
+                        string answer = GetMessage();
+                        if (answer == "yes")// если согласен
+                        {
+                            CompanionName = message;
+                            // в бесконечном цикле получаем сообщения от клиента
+                            while (true)
+                            {
+                                try
+                                {
+                                    message = GetMessage();// получаем мессаге от клиента(что-то в духе: я хочу отправить сообщение)
+                                    message = String.Format("{0}: {1}", CompanionName, message);// форматируем мессаге ф понятную для клиенткого приложения форму
+                                    //Console.WriteLine(message);
+                                    server.BroadcastMessage(message, this.CompanionName);// и отправляем мессаге рофлочелику(2ой клиент)
+                                }
+                                catch
+                                {
+                                    message = String.Format("{0}: покинул чат", CompanionName);
+                                    Console.WriteLine(message);
+                                    server.BroadcastMessage(message, this.ClientName);//отправляем мессаге рофлочелику(2ой клиент) что клиент(собеседник) ливнул
+                                    break;
+                                }
+                            }
+                        }
+                        else//если не согласен
+                        {
+                            CompanionName = null;
+                            Process();
+                        }
+                        break;
+                    case "I wanna chating with you"://сюда придёт ещё имя
+                        server.SendMessage("Someone wanna chat with you + имя которое пришло", client);
+                        string name="";//имя чела который хочет чатиться(надо будет распарсить)
+                        string answer2 = GetMessage();
+                        if (answer2 == "yes")// отправляем тому кто запрашивал ответ клиента
+                        {
+                            foreach (ClientObject clientObj in server.GetClients)// ищем в онлайне этого челика и отправляем ему запрос
+                            {
+                                if (name == clientObj.ClientName) server.SendMessage("Yes, I wanna chat too" + ClientName, clientObj.client);//ищем его имя и отправляем ответ "ДА"
+                            }
+                            CompanionName = name;// имя собеседника 
+                            // в бесконечном цикле получаем сообщения от клиента
+                            while (true)
+                            {
+                                try
+                                {
+                                    message = GetMessage();// получаем мессаге от клиента(что-то в духе: я хочу отправить сообщение)
+                                    message = String.Format("{0}: {1}", CompanionName, message);// форматируем мессаге ф понятную для клиенткого приложения форму
+                                    //Console.WriteLine(message);
+                                    server.BroadcastMessage(message, this.CompanionName);// и отправляем мессаге рофлочелику(2ой клиент)
+                                }
+                                catch
+                                {
+                                    message = String.Format("{0}: покинул чат", CompanionName);
+                                    Console.WriteLine(message);
+                                    server.BroadcastMessage(message, this.ClientName);//отправляем мессаге рофлочелику(2ой клиент) что клиент(собеседник) ливнул
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            foreach (ClientObject clientObj in server.GetClients)// тогда ищем в онлайне этого челика и отправляем ему запрос
+                            {
+                                if (name == clientObj.ClientName) server.SendMessage("No, i dont wanna chat" + ClientName, clientObj.client);//ищем его имя и отправляем ответ "НЕТ"
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(this.ClientName);
+                Close();
+            }
+        }
+
+        // чтение входящего сообщения
+        private string GetMessage()
+        {
+            byte[] data = new byte[64];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        // закрытие подключения
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ ChatServer/ServerObject.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace ChatServer
+{
+    public class Server
+    {
+        static TcpListener tcpListener; // сервер для прослушивания
+        List<ClientObject> clients = new List<ClientObject>(); // все подключения(онлайн)
+        List<(ClientObject FirstClient, ClientObject SecondClient)> ActiveChats = new List<(ClientObject FirstClient, ClientObject SecondClient)>();//активные чаты
+
+        public List<ClientObject> GetClients
+        {
+            get { return clients; }
+        }
+
+        protected internal void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+        }
+
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            ClientObject client = clients.FirstOrDefault(c => c.ClientName == id);
+            // и удаляем его из списка подключений
+            if (client != null)
+                clients.Remove(client);
+        }
+
+        // прослушивание входящих подключений
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 11111);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    switch(GetMessage(tcpClient))//cкорее всего и его нужно в отдельный поток(но это не точно) вроде там листенера нету
+                    {
+                        case "0":
+                            //регистрация
+                            //новый поток для регистрации и тд...
+                            break;
+                        case "1"://логин
+                            if (true)//(проверка логина и пароля) {создание потока для обработки клиента}
+                            {
+                                string LoginName = "распаршенный логин";
+                                ClientObject clientObject = new ClientObject(tcpClient, this, LoginName);
+                                Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                                clientThread.Start();
+                                SendMessage("login accepted", tcpClient);//+список онлайна
+                            }
+                            else
+                            {
+                                SendMessage("login unaccepted", tcpClient);
+                            }
+                            break;
+                        case "2":
+                            //список онлайна
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        private string GetMessage(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        protected internal void SendMessage(string message, TcpClient client)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+        }
+
+        // отправка сообщения микрочелу
+        protected internal void BroadcastMessage(string message, string id)// надо переделать
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].ClientName != id) 
+                {
+                    clients[i].Stream.Write(data, 0, data.Length); //передача данных
+                }
+            }
+        }
+
+        // отключение всех клиентов
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //остановка сервера
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); //отключение клиента
+            }
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
